22db1536ecde5f15bfff58fca7a6d489bdd96d44
thihuli <thiagocandidof@hotmail.com>
2020-12-23 01:17:55 -0300

    add library chai for test in mocha

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,18 +1,18 @@
-const assert = require('assert');
 const Math = require('../src/math');
+const expect = require('chai').expect;
 
 
 describe('Math class', function() {
   it('Sum two numbers', function(done) {
     const math = new Math();
 
-    assert.equal(math.sum(5, 5), 10);
+    expect(math.sum(5, 5)).to.equal(10);
     done();
   })
 
   it('Multiply two numbers', function() {
     const math = new Math();
 
-    assert.equal(math.multiply(5, 3), 15);
+    expect(math.multiply(5, 3)).to.equal(15);
   } )
 });
