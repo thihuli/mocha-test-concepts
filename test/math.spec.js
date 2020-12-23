cb180981dff7564fbe0268ac8a8be8cab4100c44
thihuli <thiagocandidof@hotmail.com>
2020-12-23 01:07:30 -0300

    add test multiply

--- test/math.spec.js
+++ test/math.spec.js
@@ -2,10 +2,17 @@ const assert = require('assert');
 const Math = require('../src/math');
 
 
-describe('Math class', function () {
-  it('Sum two numbers', function() {
+describe('Math class', function() {
+  it('Sum two numbers', function(done) {
     const math = new Math();
 
     assert.equal(math.sum(5, 5), 10);
+    done();
   })
-})
+
+  it('Multiply two numbers', function() {
+    const math = new Math();
+
+    assert.equal(math.multiply(5, 3), 15);
+  } )
+});
